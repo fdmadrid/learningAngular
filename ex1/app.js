3f16eed7e85c8fc21ba4e1beb3e2b92c6d115011
Dario <fdmadrid@gmail.com>
2018-07-12 14:58:56 +0200

    using git through a proxy

--- ex1/app.js
+++ ex1/app.js
@@ -1,26 +1,11 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('myApp', []);
 
-app.controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
-        $scope.posts = [
-            { title: 'post 1', upvotes: 5 },
-            { title: 'post 2', upvotes: 2 },
-            { title: 'post 3', upvotes: 15 },
-            { title: 'post 4', upvotes: 9 },
-            { title: 'post 5', upvotes: 4 }
-        ];
-        $scope.addPost = function () {
-            if (!$scope.title || $scope.title === '') { return; }
-            $scope.posts.push({
-                title: $scope.title,
-                link: $scope.link,
-                upvotes: 0
-            });
-            $scope.title = '';
-            $scope.link = '';
-        };
-        $scope.incrementUpvotes = function (post) {
-            post.upvotes += 1;
-        };
-    }]);
+app.controller('myCtrl', function($scope) {
+    $scope.firstname = "John";
+    $scope.lastname = "Doe";
+    $scope.quantity = "1";
+    $scope.price = "5";
+    $scope.names =[ {name:'Jani',country:'Norway'},
+                    {name:'Hege',country:'Sweden'},
+                    {name:'Kai',country:'Denmark'}];
+});
